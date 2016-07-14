30e3ed463aa083808895eea9f1b38d6b21f78047
genzouw <genzouw@gmail.com>
2016-07-15 02:03:00 +0900

    ddlを追加

---
+++ myapp/Config/Schema/ddl.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS upload_files;
+CREATE TABLE upload_files (
+    id integer not null primary key autoincrement,
+    comment text,
+    remote_address text,
+    base64_content text not null,
+    mime_type text not null,
+    file_name text not null,
+    file_size integer not null,
+    created_at datetime default null,
+    updated_at datetime default null
+);
+
